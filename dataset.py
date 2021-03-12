0df225f52ab9b323fe9650fdda96d54eac7de589
kamilwyszynski <1722989@brunel.ac.uk>
2021-03-12 19:44:06 +0000

    added offset for easier image loading

--- dataset.py
+++ dataset.py
@@ -4,13 +4,13 @@ from PIL import Image
 
 class VizWiz():
 
-    def __init__(self, path='res/vizwiz', set_option='train'):
+    def __init__(self, path='res/vizwiz', set_option='train', offset=0):
         with open(os.path.join(path, 'annotations', f'{set_option}.json')) as f:
             self.annotations = json.load(f)
 
         self.images_path = os.path.join(path, set_option)
         self.set_option = set_option
-        self.current = 0
+        self.current = offset
 
     def __iter__(self):
         return self
