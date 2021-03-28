350302d633728153463230cd5a010767facc5483
kamilwyszynski <1722989@brunel.ac.uk>
2021-03-28 15:36:20 +0100

    correct captions fixed

--- dataset.py
+++ dataset.py
@@ -18,8 +18,15 @@ class VizWiz():
     def __next__(self):
         img_name = f'VizWiz_{self.set_option}_{self.current:08d}.jpg'
         img = Image.open(os.path.join(self.images_path, img_name))
-        cap = self.annotations['annotations'][self.current]['caption']
+
+        cap = ''
+        for i in range(5):
+            cap += ' ' + self.annotations['annotations'][self.current*5+i]['caption']
 
         self.current+=1
 
         return img, cap, img_name
+
+    def get_caption(self, image):
+        img_id = int(image[13:-4])
+        return self.annotations['annotations'][img_id]['caption']
